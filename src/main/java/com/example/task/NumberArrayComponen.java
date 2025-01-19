dc8da791feee186aecb84bdfc058ef6ebafa3a0d
black32h <blackspetya@gmail.com>
2025-01-19 16:13:25 +0200

    Update NumberArrayComponen.java

--- src/main/java/com/example/task/NumberArrayComponen.java
+++ src/main/java/com/example/task/NumberArrayComponen.java
@@ -3,21 +3,26 @@ package com.example.task;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+// Компонент для роботи з властивостями з файлу конфігурації
 @Component
-@ConfigurationProperties(prefix = "number")
+@ConfigurationProperties(prefix = "number")  // Зчитуємо властивості з префіксом "number"
 public class NumberArrayComponen {
 
-    private int[] array;
+    private int[] array;  // Масив цілих чисел
 
+    // Геттер для масиву
     public int[] getArray() {
         return array;
     }
 
+    // Сеттер для масиву
     public void setArray(int[] array) {
         this.array = array;
     }
 
+    // Метод для виведення чисел масиву в консоль
     public void printNumberArray() {
+        // Перебираємо елементи масиву і виводимо їх на консоль
         for (int num : array) {
             System.out.println(num);
         }
