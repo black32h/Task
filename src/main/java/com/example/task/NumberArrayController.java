6985abc7682718ed571f85a482d0fe008578a07c
black32h <blackspetya@gmail.com>
2025-01-19 16:14:30 +0200

    Update NumberArrayController.java

--- src/main/java/com/example/task/NumberArrayController.java
+++ src/main/java/com/example/task/NumberArrayController.java
@@ -4,15 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Клас контролера, який обробляє HTTP-запити
 @RestController
 public class NumberArrayController {
 
+    // Автоматичне впровадження компонента NumberArrayComponen
     @Autowired
     private NumberArrayComponen numberArrayComponent;
 
+    // Метод для обробки GET-запиту на шлях "/numbers"
     @GetMapping("/numbers")
     public String getNumbers() {
+        // Викликаємо метод printNumberArray() для виведення чисел з масиву
         numberArrayComponent.printNumberArray();
+        
+        // Повертаємо повідомлення, що результат виведено на консоль
         return "Check your console for the output!";
     }
 }
