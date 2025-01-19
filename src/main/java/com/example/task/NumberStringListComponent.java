c8471692654d586d6f7d1866bb18b25b2748459e
black32h <blackspetya@gmail.com>
2025-01-19 16:30:02 +0200

    Update NumberStringListComponent.java

--- src/main/java/com/example/task/NumberStringListComponent.java
+++ src/main/java/com/example/task/NumberStringListComponent.java
@@ -10,20 +10,21 @@ import java.util.List;
 public class NumberStringListComponent {
 
     // Використання @Value і SpEL для отримання списку зі строки
-    @Value("#{'${numberStringList}'.split(',')}")
-    private List<String> numberStringList;
+    @Value("#{'${numberStringList}'.split(',')}") // Анотація @Value з використанням SpEL (Spring Expression Language) для отримання списку значень, розділених комами, зі строки властивостей
+    private List<String> numberStringList; // Поле для зберігання списку значень
 
     // Гетер для доступу до списку
-    public List<String> getNumberStringList() {
+    public List<String> getNumberStringList() { // Метод, який повертає список
         return numberStringList;
     }
 
     // Метод для виведення списку в консоль
-    public void printNumberStringList() {
-        if (numberStringList != null) {
-            numberStringList.forEach(System.out::println);
+    public void printNumberStringList() { 
+        if (numberStringList != null) { // Перевіряємо, чи список не є null
+            numberStringList.forEach(System.out::println); // Виводимо кожен елемент списку у консоль
         } else {
-            System.out.println("Список порожній!");
+            System.out.println("Список порожній!"); // Повідомлення, якщо список порожній
         }
     }
 }
+
