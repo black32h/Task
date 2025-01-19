e3d995ded756f802f66012903dea3470e3cab0b5
black32h <blackspetya@gmail.com>
2025-01-19 16:49:08 +0200

    Update RandomValuesController.java

--- src/main/java/com/example/task/RandomValuesController.java
+++ src/main/java/com/example/task/RandomValuesController.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController  // Позначає клас як контролер для обробки HTTP-запитів
 public class RandomValuesController {
 
-    @Autowired
+    @Autowired  // Автоматичне впровадження біну RandomValuesComponent
     private RandomValuesComponent randomValuesComponent;
 
-    @GetMapping("/randomValues")
+    // Обробник GET-запиту, який повертає випадкові значення
+    @GetMapping("/randomValues")  // Маппінг на запит за адресою "/randomValues"
     public RandomValues getRandomValues() {
+        // Повертає новий об'єкт RandomValues, заповнений значеннями з RandomValuesComponent
         return new RandomValues(randomValuesComponent.getNumber(),
                 randomValuesComponent.getRandomLong(),
                 randomValuesComponent.getUuid());
