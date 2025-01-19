3ffcc2e3459f005cc6a38acec85e1729a9e628ca
black32h <blackspetya@gmail.com>
2025-01-19 14:45:59 +0200

    Update CollectionsPropertyUtils.java

--- src/main/java/com/example/task/CollectionsPropertyUtils.java
+++ src/main/java/com/example/task/CollectionsPropertyUtils.java
@@ -5,20 +5,29 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+// Позначаємо клас як компонент Spring, що дозволяє автоматично впроваджувати його в інші класи
 @Component
+
+// Позначаємо, що цей клас буде використовувати конфігураційні властивості з префіксом "test"
 @ConfigurationProperties(prefix = "test")
 public class CollectionsPropertyUtils {
+    
+    // Поле для збереження мапи, де ключем буде String, а значенням також String
     private Map<String, String> map;
 
+    // Геттер для отримання мапи
     public Map<String, String> getMap() {
         return map;
     }
 
+    // Сеттер для встановлення мапи
     public void setMap(Map<String, String> map) {
         this.map = map;
     }
 
+    // Метод для отримання значення за ключем із мапи
     public String getValueByKey(String key) {
+        // Перевіряємо, чи мапа не пуста, і якщо не пуста, повертаємо значення за ключем
         return map != null ? map.get(key) : null;
     }
 }
