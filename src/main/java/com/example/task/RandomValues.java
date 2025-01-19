756a387206ada0e1064c81a533614f475e7602af
black32h <blackspetya@gmail.com>
2025-01-19 16:48:24 +0200

    Update RandomValues.java

--- src/main/java/com/example/task/RandomValues.java
+++ src/main/java/com/example/task/RandomValues.java
@@ -2,25 +2,28 @@ package com.example.task;
 
 public class RandomValues {
 
-    private String number;
-    private long randomLong;
-    private String uuid;
+    private String number;  // Поле для збереження випадкового числа у вигляді рядка
+    private long randomLong;  // Поле для збереження випадкового довгого числа
+    private String uuid;  // Поле для збереження випадкового UUID
 
+    // Конструктор для ініціалізації полів
     public RandomValues(String number, long randomLong, String uuid) {
-        this.number = number;
-        this.randomLong = randomLong;
-        this.uuid = uuid;
+        this.number = number;  // ініціалізація поля number
+        this.randomLong = randomLong;  // ініціалізація поля randomLong
+        this.uuid = uuid;  // ініціалізація поля uuid
     }
 
-    // Геттеры
+    // Геттер для отримання значення поля number
     public String getNumber() {
         return number;
     }
 
+    // Геттер для отримання значення поля randomLong
     public long getRandomLong() {
         return randomLong;
     }
 
+    // Геттер для отримання значення поля uuid
     public String getUuid() {
         return uuid;
     }
